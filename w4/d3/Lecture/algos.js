24c33d44b7c7decbd3fe6d2bf06eb525b8eaca41
Kyle <reimers95@gmail.com>
2023-10-19 20:56:15 -0500

    lecture

--- w4/d3/Lecture/algos.js
+++ w4/d3/Lecture/algos.js
@@ -1,4 +1,33 @@
 // 1) Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Step 1 - define a function
+// Step 2 - Has 2 parameters
+// Step 3 -- define the sum
+// step 3.5 - Check if a and b are equal 
+// Step 4 - Check which is the greater number = 2 if statements
+// Step 5 - Counting the in-betweens 
+// Step 6 - add each number to sum
+// Step 7 - Return sum
+
+function addSum(a,b)  {
+    let sum = 0
+    if(a == b) {
+        return a
+    }
+    if(a>b) {
+        for(var i = b; i <= a; i++) {
+            sum += i
+        }
+    }else{
+        for(var i = a; i <=b; i++){
+            sum += i
+        }
+    }
+    return sum
+}
+
+console.log(addSum(6,28))
+
+
 
 
 
@@ -11,10 +40,16 @@
 
 
 
+
+
+
 // 3)
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 // arr = [1, 2, 3, 4]
 // => 1 * 2 * 3 * 4 = 24
+
+
+
 
 
 
@@ -26,7 +61,13 @@
 
 
 
+
+
+
 // 5) Build a function that returns an array of any given array in reverse. return another array in reverse order
+
+
+
 
 
 
--- w4/d3/OH/script.js
+++ w4/d3/OH/script.js
@@ -15,20 +15,33 @@ async function chooseYou(event){
     <img src="${data.sprites.front_default}" alt="${data.name}" width=150></div>`
 }
 
+async function pickOne(banana){
+    console.log(banana,"******************")
+    let response = await fetch(banana)
+    let data = await response.json()
+    // console.log(data)
+    // console.log(data.name)
+    // console.log(data.sprites.front_default)
+    let poke = document.querySelector('.pokemon')
+    poke.innerHTML += `<div class="card"><h2>Name: ${data.name}</h2>
+    <img src="${data.sprites.front_default}" alt="${data.name}" width=150></div>`
+}
+
 
 
 async function getPokemon(event){
     event.preventDefault()
     let input = document.querySelector('#number').value
     // console.log(input)
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${input}&offset=0`)
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${input}&offset=5`)
     let data = await response.json()
     console.log(data)
     let poke = document.querySelector('.pokemon')
     for(let i =0; i < data.results.length; i++){
         console.log(data.results[i].name)
-        let name= data.results[i].name
-        poke.innerHTML += `<div class="card"><h2>Name: ${name}</h2></div>`
+        let url= data.results[i].url
+        console.log(url, "*****************")
+        pickOne(url)
     }
 
 }
